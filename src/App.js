a821bfdcfb2452b02bea0fdb929afde78db6c85f
HungFai Koo <jhk888@yahoo.com>
2022-10-12 09:53:46 -0700

    add css file and link semantic-ui indexJs file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import CommentDetail from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
+import "./style/App.css";
 
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          content="I like the subject"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 5:00PM"
+          content="I like the writing"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
