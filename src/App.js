83be8d5513612d252e8019269ba943ab1114d48a
HungFai Koo <jhk888@yahoo.com>
2022-10-20 13:24:46 -0700

    refactor seasonDisplay file and solved gps location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+import SeasonDisplay from "./SeasonDisplay";
 
-    // This is the only time we do direct assignment
-    //to this.state
-    this.state = { lat: null, errorMessage: "" };
-  }
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) =>
@@ -23,7 +20,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lav} />;
     }
     return <div>Loading!</div>;
   }
